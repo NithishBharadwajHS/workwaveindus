d86ec7206d040359543ae0da6a0efc75f7fbf48d
NithishBharadwajHS <nithish.lab18@gmail.com>
2025-02-10 14:39:53 +0530

    Add files via upload

---
+++ register.js
@@ -0,0 +1,17 @@
+
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-app.js";
+
+ 
+ const firebaseConfig = {
+   apiKey: "AIzaSyCxX88GgM9NPgnPCtWXhmbAxB5RE20oYiM",
+   authDomain: "work-wave-2025.firebaseapp.com",
+   projectId: "work-wave-2025",
+   storageBucket: "work-wave-2025.firebasestorage.app",
+   messagingSenderId: "1051866340329",
+   appId: "1:1051866340329:web:18bf2c9e79b96cc74ec978",
+
+ };
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+ 
